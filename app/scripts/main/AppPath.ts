6686c8edadb33c0096a861761b6b8c44f117518f
Cindy GERINTE <cge@sirdata.fr>
2016-11-26 11:33:04 +0100

    gitignore + AppPath

--- app/scripts/main/AppPath.ts
+++ app/scripts/main/AppPath.ts
@@ -2,5 +2,9 @@
 export const APP_PATH = {
     HOME: 'dashboard',
     ACCOUNT: 'account',
-    CONTACT: 'contact'
+    CONTACT: 'contact',
+    STATS_DAILY: 'stats/daily',
+    STATS_MONTHLY: 'stats/monthly',
+    TAGS: 'tags',
+    INCOMES: 'incomes',
   };
